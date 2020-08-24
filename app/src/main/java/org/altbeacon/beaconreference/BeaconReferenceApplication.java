0b80611de47876310205d8e89ffad8526d2d32cd
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2020-08-24 12:11:54 -0300

    Anda pero medio medio

--- app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
+++ app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
@@ -6,20 +6,34 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
 import android.os.Build;
 import android.util.Log;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
+
+import java.util.Arrays;
 
 /**
  * Created by dyoung on 12/13/13.
@@ -31,9 +45,14 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
     private boolean haveDetectedBeaconsSinceBoot = false;
     private MonitoringActivity monitoringActivity = null;
     private String cumulativeLog = "";
+    private ScanCallback scanCallback;
+    private int rssi;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onCreate() {
         super.onCreate();
+
+
         BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
 
         // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
@@ -55,7 +74,6 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
         // communicate to users that your app is using resources in the background.
         //
 
-
         Notification.Builder builder = new Notification.Builder(this);
         builder.setSmallIcon(R.drawable.ic_launcher);
         builder.setContentTitle("Scanning for Beacons");
@@ -73,6 +91,9 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
             notificationManager.createNotificationChannel(channel);
             builder.setChannelId(channel.getId());
         }
+
+
+
         beaconManager.enableForegroundServiceScanning(builder.build(), 456);
 
         // For the above foreground scanning service to be useful, you need to disable
@@ -83,20 +104,58 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
         beaconManager.setBackgroundBetweenScanPeriod(0);
         beaconManager.setBackgroundScanPeriod(1100);
 
-        Log.d(TAG, "setting up background monitoring for beacons and power saving");
-        // wake up the app when a beacon is seen
-        Region region = new Region("backgroundRegion",
-                null, null, null);
+        // The following code block effectively disables beacon scanning in the foreground service
+        // to save battery.  Do not include this code block if you want to detect beacons
+
+//        beaconManager.getBeaconParsers().clear(); // clearning all beacon parsers ensures nothing matches
+//        beaconManager.setBackgroundBetweenScanPeriod(Long.MAX_VALUE);
+//        beaconManager.setBackgroundScanPeriod(0);
+//        beaconManager.setForegroundBetweenScanPeriod(Long.MAX_VALUE);
+//        beaconManager.setForegroundScanPeriod(0);
+
+
+        // The following code block activates the foreground service by starting background scanning
+        Region region = new Region("dummy-region",
+                Identifier.parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"), null, null);
         regionBootstrap = new RegionBootstrap(this, region);
+
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+        // This code block starts beacon transmission
+
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118) // Radius Networks.  Change this for other beacon layouts
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(0l)) // Remove this for beacon layouts without d: fields
+                .build();
+
+        // Change the layout below for other beacon types
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertisement start failed with code: "+errorCode);
+            }
+
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+            }
+        });
 
         // simply constructing this class and holding a reference to it in your custom Application
         // class will automatically cause the BeaconLibrary to save battery whenever the application
         // is not visible.  This reduces bluetooth power usage by about 60%
-        backgroundPowerSaver = new BackgroundPowerSaver(this);
+
 
         // If you wish to test beacon detection in the Android Emulator, you can use code like this:
         // BeaconManager.setBeaconSimulator(new TimedBeaconSimulator() );
         // ((TimedBeaconSimulator) BeaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();
+
     }
 
     public void disableMonitoring() {
@@ -182,7 +241,7 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
     private void logToDisplay(String line) {
         cumulativeLog += (line + "\n");
         if (this.monitoringActivity != null) {
-            this.monitoringActivity.updateLog(cumulativeLog);
+            this.monitoringActivity.updateLog(line);
         }
     }
 
